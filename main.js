61a5458deb6714f2d80ba74cdd200dd9b03f4d3a
jonathan <97814319+jonathansfx@users.noreply.github.com>
2023-01-03 16:41:18 -0700

    finish

---
+++ main.js
@@ -0,0 +1,104 @@
+// Same Name:
+// let username = prompt("Please enter your name");
+// if (username === "Jonathan") {
+    // alert("Wow, we have the same name.");
+// } else {
+    // alert("We don't have the same name.");
+// }
+
+// Carnival Ride Height Restriction:
+// let height = +prompt("Please enter your height");
+// if (height < 82) {
+    // alert("Too short for a ride.");
+// } else {
+    // alert("All Aboard!");
+// }
+
+// Analyze Number:
+// let num = +prompt("Please enter a number:");
+// if (num < 0) {
+    // alert("negative");
+// } else if (num > 0) {
+    // alert("positive");
+// } else {
+    // alert("zero");
+// }
+
+// Course Reccomendation:
+// let grade = +prompt("Please enter your grade.");
+// if (grade >= 90) {
+    // alert("You will do well at the next level.");
+// } else if (grade >= 70 && grade <= 89) {
+    // alert("You should do well at the next level.");
+// } else if (grade >= 50 && grade <= 69) {
+    // alert("the next level may be difficult for you.");
+// } else {
+    // alert("You may not go to the next level.")
+// }
+
+// Absolute Value:
+// let num = +prompt("Please enter a number.")
+// if (num >= 0) {
+    // alert(num);
+// } else if (num < 0) {
+// alert(num * -1);
+// }
+
+// Even Number:
+// let num = +prompt("Please enter a number");
+// if (num % 2 == 0) {
+    // alert ("true");
+// } else {
+    // alert ("false");
+// }
+
+// Check Divisibility:
+// let num = +prompt("Please enter a number.");
+// let div = +prompt("Please enter a divisor.");
+// if (num % div == 0) {
+    // alert("true");
+// } else {
+    // alert("false");
+// }
+
+// Point Location:
+// let x = +prompt("Please enter the X coordinate:");
+// let y = +prompt("Please enter a y coordiniate:");
+// if( x == 0 && y == 0) {
+    // alert("origin");
+// } else if (x > 0 && y > 0) {
+    // alert("quadrant 1");
+// } else if (x < 0 && y > 0) {
+    // alert("quadrant 2");
+// } else if (x < 0 && y < 0) {
+    // alert("quadrant 3");
+// } else if (x > 0 && y < 0) {
+    // alert("quadrant 4");
+// } else if (x == 0) {
+    // alert("y-axis");
+// } else if (y == 0) {
+    // alert("x-axis");
+// }
+
+// Ordinal Adjectives:
+// let num = +prompt("Enter a number.");
+// if (num == 1) {
+    // alert("1st");
+// } else if (num == 2) {
+    // alert("2nd");
+// } else if (num == 3) {
+    // alert("3rd");
+// } else {
+    // alert(`${num}th`);
+// }
+
+// Min Payment:
+// let balance = +prompt("enter your balance");
+// let minimumPayment = balance * 0.021;
+// if (minimumPayment < 10) {
+//   minimumPayment = 10;
+// }
+// if (minimumPayment > balance) {
+//   minimumPayment = balance;
+// }
+// alert(minimumPayment);
